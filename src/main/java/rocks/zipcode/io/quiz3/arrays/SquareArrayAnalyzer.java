9eafa4de351c782fdafd55669e9d019e5495aa40
Cristina McClintock <cristinamcclintock@zipcoders-MacBook-Pro.local>
2019-03-11 13:21:57 -0400

    sad

--- src/main/java/rocks/zipcode/io/quiz3/arrays/SquareArrayAnalyzer.java
+++ src/main/java/rocks/zipcode/io/quiz3/arrays/SquareArrayAnalyzer.java
@@ -1,10 +1,27 @@
 package rocks.zipcode.io.quiz3.arrays;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * @author leon on 09/12/2018.
  */
 public class SquareArrayAnalyzer {
+
+
     public static Boolean compare(Integer[] input, Integer[] squaredValues) {
-        return null;
+        ArrayList<Integer> inputToArrayList = new ArrayList<Integer>();
+       ArrayList<Integer> squaredValuesArrayList = new ArrayList<Integer>(Arrays.asList(squaredValues));
+
+        for(int i = 0; i < inputToArrayList.size()-1; i++){
+            inputToArrayList.add(input[i] * input[i]);
+        }
+
+        for(int i = 0; i < inputToArrayList.size()-1; i++){
+            if(!squaredValuesArrayList.contains(inputToArrayList.get(i))){
+                return false;
+            }
+        }
+        return true;
     }
 }
--- src/main/java/rocks/zipcode/io/quiz3/arrays/WaveGenerator.java
+++ src/main/java/rocks/zipcode/io/quiz3/arrays/WaveGenerator.java
@@ -1,10 +1,42 @@
 package rocks.zipcode.io.quiz3.arrays;
 
+import java.util.ArrayList;
+
 /**
  * @author leon on 09/12/2018.
  */
 public class WaveGenerator {
     public static String[] wave(String str) {
-        return null;
+//        str = str.toLowerCase();
+//        ArrayList<String> ar = new ArrayList<>();
+//        StringBuilder c = new StringBuilder(str);
+//
+//       StringBuilder sb = new StringBuilder();
+//
+//        for(int i = 0; i < str.length(); i++){
+//            sb.append(str.substring(i).toUpperCase());
+//        }
+//        return sb.toString().split("");
+        String[] stringarray = str.split("");
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < stringarray.length; i++){
+            for(int k = 0; k < stringarray[i].length(); k++){
+
+                String string = stringarray[k];
+               Character.toUpperCase(string.charAt(k));
+                sb.append(stringarray[k]);
+            }
+        }
+        return sb.toString().split("");
+
     }
+
+//    public String capitalizeOneLetter(String str, int index ){
+//       str = str.substring(index).toUpperCase();
+//        return str;
+//    }
+
+
 }
