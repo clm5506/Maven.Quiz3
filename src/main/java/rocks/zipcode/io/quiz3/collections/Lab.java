00e0a49dcf72fc03f3dfd6962331e87e8d0e2ca9
Cristina McClintock <cristinamcclintock@zipcoders-MacBook-Pro.local>
2019-03-11 11:58:11 -0400

    checking in more

--- src/main/java/rocks/zipcode/io/quiz3/collections/Lab.java
+++ src/main/java/rocks/zipcode/io/quiz3/collections/Lab.java
@@ -4,14 +4,17 @@ package rocks.zipcode.io.quiz3.collections;
  * @author leon on 10/12/2018.
  */
 public class Lab {
+    String nameOfLab;
+
     public Lab() {
         this(null);
     }
 
     public Lab(String labName) {
+        this.nameOfLab = labName;
     }
 
     public String getName() {
-        return null;
+        return this.nameOfLab;
     }
 }
--- src/main/java/rocks/zipcode/io/quiz3/collections/Student.java
+++ src/main/java/rocks/zipcode/io/quiz3/collections/Student.java
@@ -2,31 +2,70 @@ package rocks.zipcode.io.quiz3.collections;
 
 import rocks.zipcode.io.quiz3.objectorientation.enums.LabStatus;
 
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author leon on 10/12/2018.
  */
 public class Student {
+
+    Map<Lab, LabStatus> recordOfLabs;
+
     public Student() {
-        this(null);
+       // this(null);
+
+        this.recordOfLabs = new HashMap<>();
     }
 
     public Student(Map<Lab, LabStatus> map) {
+        this.recordOfLabs = map;
     }
+
 
     public Lab getLab(String labName) {
-        return null;
+
+        for (Map.Entry<Lab, LabStatus> entry : recordOfLabs.entrySet()) {
+            if (entry.getKey().getName().equals(labName)) {
+                return entry.getKey();
+            }
+        } throw new UnsupportedOperationException();
+
+
     }
 
+
+
     public void setLabStatus(String labName, LabStatus labStatus) {
+        Lab l = getLab(labName);
+        recordOfLabs.put(l,labStatus);
+
     }
 
 
     public void forkLab(Lab lab) {
+        recordOfLabs.put(lab,LabStatus.PENDING);
     }
 
     public LabStatus getLabStatus(String labName) {
-        throw new UnsupportedOperationException("Method not yet implemented");
+
+        for(Map.Entry<Lab, LabStatus> entry : recordOfLabs.entrySet()) {
+            if (entry.getKey().getName().equals(labName)) {
+                return entry.getValue();
+            }
+        }
+        return null;
+
+    }
+
+    public String toString() {
+        String ans = "";
+
+        for(Map.Entry<Lab, LabStatus> entry : recordOfLabs.entrySet()) {
+            ans = ans + entry.getKey().getName() + " > " + entry.getValue().toString()
+                    .replace("[","").replace("]","") + "\n";
+        }
+
+
+        return ans;
     }
 }
